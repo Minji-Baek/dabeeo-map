87acdcf6e87fbb407b19bedfe995fe1f26e6fc97
minjiBaek <emilyback@naver.com>
2023-09-12 00:13:53 +0900

    main view setting

---
+++ src/views/MapView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MapImage from '../components/MapImage.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+    <h1>Dabeeo Map Project</h1>
+  </header>
+    <MapImage />
+  </div>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  font-size: 35px;
+  font-weight: 700;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+</style>
